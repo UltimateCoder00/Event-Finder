22d79795399ca44b450cafc6a28ee155d3c847d4
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 17:36:16 +0100

    Remove private method optimal_price_generator test

--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -7,13 +7,10 @@ describe Ticket do
     expect(ticket.price).to be_a Integer
   end
 
-  it '#optimal_price_generator' do
-    expect(ticket.optimal_price_generator).to be_a Integer
-  end
-
   it '#edit_price' do
     ticket_price = 100
     ticket2 = described_class.new(ticket_price)
+    expect(ticket2.price).to eq ticket_price
 
     new_ticket_price = 200
     ticket2.edit_price(new_ticket_price)
