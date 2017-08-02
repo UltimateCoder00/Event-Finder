491267b29d1428753cbbb0cc7740b5c0ad844536
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 17:35:46 +0100

    Make optimal_price_generator method private

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -7,11 +7,13 @@ class Ticket
     @price = price == -1 ? optimal_price_generator : @price = price
   end
 
-  def optimal_price_generator
-    rand(0..MAXIMUM_TICKET_PRICE_IN_PENNIES)
-  end
-
   def edit_price(new_price)
     @price = new_price
   end
+
+  private
+  def optimal_price_generator
+    rand(0..MAXIMUM_TICKET_PRICE_IN_PENNIES)
+  end
+  
 end
