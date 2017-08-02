de75f7ddcc4c73d5414a1fd0f8ef3e30cf53a597
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 16:56:50 +0100

    Add ticket price check test

--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -1,5 +1,9 @@
 require 'ticket'
 
-describe 'Ticket' do
+describe Ticket do
+  subject(:ticket) {described_class.new}
 
+  it 'Checks the price of the ticket' do
+    expect(ticket.price).to be_a Integer
+  end
 end
