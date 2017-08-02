cf77305bd1530fd0d69d11922a9eeb480153957f
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 16:59:54 +0100

    Add MAXIMUM_TICKET_PRICE_IN_PENNIES constant variable

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,11 +1,13 @@
 class Ticket
   attr_reader :price
 
+  MAXIMUM_TICKET_PRICE_IN_PENNIES = 100000
+
   def initialize
-    @price = declare_price
+    @price = declare_price(MAXIMUM_TICKET_PRICE_IN_PENNIES)
   end
 
-  def declare_price
-    rand(0..10000)
+  def declare_price(max_price)
+    rand(0..max_price)
   end
 end
