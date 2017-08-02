f2900b400bca278d9644b8a8b6199c2c2b2e9846
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 17:03:34 +0100

    Add declare_price method test

--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -6,4 +6,8 @@ describe Ticket do
   it 'Checks the price of the ticket' do
     expect(ticket.price).to be_a Integer
   end
+
+  it '#declare_price' do
+    expect(ticket.declare_price(Ticket::MAXIMUM_TICKET_PRICE_IN_PENNIES)).to be_a Integer
+  end
 end
