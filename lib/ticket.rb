fb3810c8e2e754996394b2f778c2682b2df13164
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 17:31:11 +0100

    Remove redundant add_price method

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -4,11 +4,7 @@ class Ticket
   MAXIMUM_TICKET_PRICE_IN_PENNIES = 100000
 
   def initialize(price = -1)
-    @price = add_price(price)
-  end
-
-  def add_price(price)
-    price == -1 ? optimal_price_generator : @price = price
+    @price = price == -1 ? optimal_price_generator : @price = price
   end
 
   def optimal_price_generator
