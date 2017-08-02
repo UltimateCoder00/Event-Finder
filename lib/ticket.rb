96629aa62e5e93b1bb62d4f84664119c7080e065
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 17:09:03 +0100

    Add class functionality for user to add price to ticket

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -3,8 +3,13 @@ class Ticket
 
   MAXIMUM_TICKET_PRICE_IN_PENNIES = 100000
 
-  def initialize
-    @price = declare_price(MAXIMUM_TICKET_PRICE_IN_PENNIES)
+  def initialize(price = -1)
+    @price = add_price(price)
+
+  end
+
+  def add_price(price)
+    price == -1 ? declare_price(MAXIMUM_TICKET_PRICE_IN_PENNIES) : @price = price
   end
 
   def declare_price(max_price)
