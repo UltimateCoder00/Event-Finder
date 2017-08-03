8fa56c8c02d72b5a66ebf878e438e41e076ff8a4
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-03 15:51:03 +0100

    Add initialization method and id & tickets instance variables class features and tests

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,9 @@
 class Event
+  attr_reader :id, :tickets
+
+  def initialize
+    @id = rand(0..1000000)
+    @tickets = []
+  end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,4 +3,8 @@ require 'event'
 describe Event do
   subject(:event) {described_class.new}
 
+  it 'Initialization' do
+    expect(event.id).to be_a Integer
+    expect(event.tickets).to be_a Array
+  end
 end
