1b319f2fdb274b27d1826e764333ebc3bbd7c4a4
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-03 17:04:34 +0100

    Add class initialization method and test

--- lib/locations.rb
+++ lib/locations.rb
@@ -1,3 +1,8 @@
 class Locations
+  attr_reader :coordinates
+
+  def initialize
+    @coordinates = Array.new(21) { Array.new(21) }
+  end
 
 end
--- spec/locations_spec.rb
+++ spec/locations_spec.rb
@@ -4,6 +4,6 @@ describe Locations do
   subject(:locations) {described_class.new}
 
   it 'Initialization' do
-
+    expect(locations.coordinates).to be_a Array
   end
 end
