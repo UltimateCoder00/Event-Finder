cde228035c163a91f5f0cf93bcde8c02450aac38
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-06 13:29:22 +0100

    Add adding_events_algorithm private method

--- lib/locations.rb
+++ lib/locations.rb
@@ -2,7 +2,22 @@ class Locations
   attr_reader :coordinates
 
   def initialize
-    @coordinates = Array.new(21) { Array.new(21) }
+    @coordinates = adding_events_algorithm
+  end
+
+  private
+  def adding_events_algorithm
+    array = Array.new(21) { Array.new(21) }
+
+    array.each do |x|
+      x.map! do |y|
+        if [true, false].sample
+          y = Event.new
+        end
+      end
+    end
+
+    array
   end
 
 end
