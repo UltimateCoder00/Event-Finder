4c22c11ef2a5e83ef7d876bfd89e4c7a80b48f4b
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-06 16:35:30 +0100

    Add closest_events_to method and test

--- lib/event_finder.rb
+++ lib/event_finder.rb
@@ -1,8 +1,13 @@
 require_relative 'locations'
 
 class EventFinder
+  attr_reader :event_locations
 
   def initialize
     @event_locations = Locations.new
+  end
+
+  def closest_events_to(coordinateX, coordinateY)
+    @event_locations.coordinates[coordinateX+10][coordinateY+10]
   end
 end
--- spec/event_finder_spec.rb
+++ spec/event_finder_spec.rb
@@ -6,4 +6,8 @@ describe EventFinder do
   it 'Initialization' do
     expect(event_finder.event_locations).to be_a Locations
   end
+
+  it '#closest_events_to' do
+    expect(event_finder.closest_events_to(4, 2)).to be_a(Event).or eq(nil)
+  end
 end
