950f445866e12d142cf7a10dde6b7dd6fed9f016
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-06 18:32:55 +0100

    Add proximity_scanner private method

--- lib/event_finder.rb
+++ lib/event_finder.rb
@@ -8,6 +8,31 @@ class EventFinder
   end
 
   def closest_events_to(coordinateX, coordinateY)
+    array = proximity_scanner(coordinateX, coordinateY)
+    require 'pry'; binding.pry
+
+
     @event_locations.coordinates[coordinateX+10][coordinateY+10]
+  end
+
+  private
+
+  def proximity_scanner(coordinateX, coordinateY)
+    array = []
+
+    for i in 0...21
+      for j in 0...21
+        if @event_locations.coordinates[i][j] == nil
+          next
+        else
+          x_length = (coordinateX+10 - i).abs
+          y_length = (coordinateY+10 - j).abs
+
+          array << [x_length+y_length,i,j]
+        end
+      end
+    end
+
+    array
   end
 end
