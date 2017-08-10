fc70ccc9f91d518390bdabfac91aa5de3bca22b1
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-10 15:14:46 +0100

    Remove magic numbers

--- lib/locations.rb
+++ lib/locations.rb
@@ -2,6 +2,8 @@ require_relative 'event'
 
 class Locations
   attr_reader :coordinates
+  @@x_coordinate_range = 21
+  @@y_coordinate_range = 21
 
   def initialize
     @coordinates = adding_events_algorithm
@@ -9,7 +11,7 @@ class Locations
 
   private
   def adding_events_algorithm
-    array = Array.new(21) { Array.new(21) }
+    array = Array.new(@@x_coordinate_range) { Array.new(@@y_coordinate_range) }
 
     array.each do |x|
       x.map! { Event.new if [true, false].sample }
