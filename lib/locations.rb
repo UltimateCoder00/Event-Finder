6cc58873e051ff6350e6b98911c8dadb6dd5a7f0
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-10 15:18:33 +0100

    Turn class global variables to constants

--- lib/locations.rb
+++ lib/locations.rb
@@ -2,8 +2,8 @@ require_relative 'event'
 
 class Locations
   attr_reader :coordinates
-  @@x_coordinate_range = 21
-  @@y_coordinate_range = 21
+  X_COORDINATE_RANGE = 21
+  Y_COORDINATE_RANGE = 21
 
   def initialize
     @coordinates = adding_events_algorithm
@@ -11,7 +11,7 @@ class Locations
 
   private
   def adding_events_algorithm
-    array = Array.new(@@x_coordinate_range) { Array.new(@@y_coordinate_range) }
+    array = Array.new(X_COORDINATE_RANGE) { Array.new(Y_COORDINATE_RANGE) }
 
     array.each do |x|
       x.map! { Event.new if [true, false].sample }
